b919461a2a5a95f86fde571042381b53b5ec2674
Rares-Andrei Cotoi <rares.cotoi@yahoo.com>
2022-12-16 21:02:04 +0200

    Home screen 3

--- app/src/main/java/com/example/wall_y/MainActivity.java
+++ app/src/main/java/com/example/wall_y/MainActivity.java
@@ -2,27 +2,41 @@ package com.example.wall_y;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.graphics.LinearGradient;
 import android.graphics.RadialGradient;
 import android.graphics.Shader;
 import android.os.Bundle;
 import android.text.TextPaint;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+    public static final String SWITCH1 = "switch1";
+    ImageView logosus;
+    TextView nume;
+    TextView networth;
+    TextView suma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView logosus = (ImageView) findViewById(R.id.imageView5);
-        TextView nume = (TextView) findViewById(R.id.textView);
-        TextView networth = (TextView) findViewById(R.id.textView2);
-        TextView suma = (TextView) findViewById(R.id.textView4);
+        logosus = (ImageView) findViewById(R.id.imageView5);
+        nume = (TextView) findViewById(R.id.textView);
+        networth = (TextView) findViewById(R.id.textView2);
+        suma = (TextView) findViewById(R.id.textView4);
         float balanta = 0;
-        setTextViewColor (suma, getResources().getColor(R.color.violet), getResources().getColor(R.color.crem));
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();
         suma.setText(String.valueOf(balanta));
+        setTextViewColor(suma, getResources().getColor(R.color.violet), getResources().getColor(R.color.crem));
     }
         private void setTextViewColor (TextView suma, int...color){
             TextPaint paint = suma.getPaint();
@@ -30,5 +44,13 @@ public class MainActivity extends AppCompatActivity {
             Shader shader = new LinearGradient(0, 0, width, suma.getTextSize(), color, null, Shader.TileMode.CLAMP);
             suma.getPaint().setShader(shader);
             suma.setTextColor(color[0]);
+        }
+
+        public void saveData(){
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            editor.putString(TEXT, suma.getText().toString());
+            //editor.putBoolean(SWITCH1, )
         }
 }
